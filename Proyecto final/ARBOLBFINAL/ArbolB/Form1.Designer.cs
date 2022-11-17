35513fbea3ae94e063a02147cae6a7c131548852
AngeLSanchez210 <102553465+AngeLSanchez210@users.noreply.github.com>
2022-11-17 16:19:17 -0600

    Proyecto final arreglado

---
+++ Proyecto final/ARBOLBFINAL/ArbolB/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace ArbolB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.txtInsertar = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtEliminar = new System.Windows.Forms.TextBox();
+            this.txtMostrar = new System.Windows.Forms.TextBox();
+            this.mostrarPaginas = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mostrarPaginas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnInsertar);
+            this.groupBox1.Controls.Add(this.txtInsertar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 173);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Insertar clave";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnInsertar.Location = new System.Drawing.Point(81, 114);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(100, 35);
+            this.btnInsertar.TabIndex = 1;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // txtInsertar
+            // 
+            this.txtInsertar.Location = new System.Drawing.Point(81, 55);
+            this.txtInsertar.Name = "txtInsertar";
+            this.txtInsertar.Size = new System.Drawing.Size(100, 29);
+            this.txtInsertar.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnBuscar);
+            this.groupBox2.Controls.Add(this.txtBuscar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(386, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(295, 173);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buscar clave";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBuscar.Location = new System.Drawing.Point(81, 114);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 35);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(81, 55);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(100, 29);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnEliminar);
+            this.groupBox3.Controls.Add(this.txtEliminar);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(771, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(295, 173);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Eliminar clave";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEliminar.Location = new System.Drawing.Point(81, 114);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 35);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtEliminar
+            // 
+            this.txtEliminar.Location = new System.Drawing.Point(81, 55);
+            this.txtEliminar.Name = "txtEliminar";
+            this.txtEliminar.Size = new System.Drawing.Size(100, 29);
+            this.txtEliminar.TabIndex = 0;
+            // 
+            // txtMostrar
+            // 
+            this.txtMostrar.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtMostrar.Location = new System.Drawing.Point(12, 206);
+            this.txtMostrar.Multiline = true;
+            this.txtMostrar.Name = "txtMostrar";
+            this.txtMostrar.Size = new System.Drawing.Size(1155, 743);
+            this.txtMostrar.TabIndex = 3;
+            this.txtMostrar.TextChanged += new System.EventHandler(this.txtMostrar_TextChanged);
+            // 
+            // mostrarPaginas
+            // 
+            this.mostrarPaginas.Location = new System.Drawing.Point(245, 206);
+            this.mostrarPaginas.Name = "mostrarPaginas";
+            this.mostrarPaginas.Size = new System.Drawing.Size(922, 604);
+            this.mostrarPaginas.TabIndex = 4;
+            this.mostrarPaginas.TabStop = false;
+            this.mostrarPaginas.Click += new System.EventHandler(this.mostrarPaginas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 811);
+            this.Controls.Add(this.mostrarPaginas);
+            this.Controls.Add(this.txtMostrar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mostrarPaginas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button btnInsertar;
+        private TextBox txtInsertar;
+        private GroupBox groupBox2;
+        private Button btnBuscar;
+        private TextBox txtBuscar;
+        private GroupBox groupBox3;
+        private Button btnEliminar;
+        private TextBox txtEliminar;
+        private TextBox txtMostrar;
+        private PictureBox mostrarPaginas;
+    }
+}
---
+++ Proyecto final/ARBOLBFINAL/ArbolB/Form1.cs
@@ -0,0 +1,110 @@
+namespace ArbolB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArbolB arbolb = new ArbolB();
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //con esta variable se encuentra la ubicacion de documentos del ususario
+                //y se le adiciona el nombre de una carpeta llamada Arbol B
+                string carpeta = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Arbol B/";
+
+                if (!Directory.Exists(carpeta))//se verifica si este directorio existe
+                {
+                    Directory.CreateDirectory(carpeta);//si no existe se crea el directorio
+                }
+                else if (Directory.Exists(carpeta))//se verifica si el directorio existe osea si la carpeta existe 
+                {
+                    Directory.Delete(carpeta, true);//se elimina la carpeta, juntos con todos los archivos que contenga
+                                                    //para evitar la acumulacion de archivos basura
+                    Directory.CreateDirectory(carpeta);//Se crea de nuevo la carpeta vacia
+                }
+                txtMostrar.Text = "";
+                arbolb.Insert(Int32.Parse(txtInsertar.Text));
+                mostrarPaginas.Refresh();//con esta propiedad se limpia el picture box
+                txtMostrar.Text =arbolb.Mostrar(ref mostrarPaginas, this.Font);//se manda como referencia
+                                                                                //el pictures box y la fuente actual del sistema
+                txtInsertar.Text = "";
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show("El formato del dato es incorrecto \n ingrese numeros enteros " + E , "ERROR DE INSERCION", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtInsertar.Text = "";
+            }
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtMostrar.Text = "";
+                if (arbolb.Buscar(Int32.Parse(txtBuscar.Text)) == true)
+                    MessageBox.Show("La clave " + txtBuscar.Text + " esta presente en el arbol", "INFORMACION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("La clave no se encuntra", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtMostrar.Text = arbolb.Mostrar(ref mostrarPaginas, this.Font);
+                txtBuscar.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("El formato del dato es incorrecto \n ingrese numeros enteros", "ERROR AL BUSCAR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtBuscar.Text = "";
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //con esta variable se encuentra la ubicacion de documentos del ususario
+                //y se le adiciona el nombre de una carpeta llamada Arbol B
+                string carpeta = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/Arbol B/";
+
+                if (!Directory.Exists(carpeta))//se verifica si este directorio existe
+                {
+                    Directory.CreateDirectory(carpeta);//si no existe se crea el directorio
+                }
+                else if (Directory.Exists(carpeta))//se verifica si el directorio existe osea si la carpeta existe 
+                {
+                    Directory.Delete(carpeta, true);//se elimina la carpeta, juntos con todos los archivos que contenga
+                                                    //para evitar la acumulacion de archivos basura
+                    Directory.CreateDirectory(carpeta);//Se crea de nuevo la carpeta vacia
+                }
+                txtMostrar.Text = "";
+                arbolb.Borrar(Int32.Parse(txtEliminar.Text));
+                mostrarPaginas.Refresh();//con esta propiedad se limpia el picture box
+                txtMostrar.Text = arbolb.Mostrar(ref mostrarPaginas, this.Font);//se manda como referencia
+                                                                 //el pictures box y la fuente actual del sistema
+                txtEliminar.Text = "";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("El formato del dato es incorrecto \n ingrese numeros enteros", "ERROR AL ELIMINAR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtEliminar.Text = "";
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //arbolb.dibujarPaginas(null,30,30,this.Font);
+        }
+
+        private void txtMostrar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mostrarPaginas_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
